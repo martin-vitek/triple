653e47d4d99c04c7b33db0d953520e5c85be0c23
canlab-cz <koupy@canlab.cz>
2020-01-09 14:52:16 +0100

    util params

--- utility/main.c
+++ utility/main.c
@@ -56,7 +56,8 @@ static char ttypath [TTYPATH_LENGTH];
 static void print_version (char *prg);
 static void print_usage (char *prg);
 static void child_handler (int signum);
-static const char *look_up_can_speed (int speed);
+static int look_up_can_speed (int speed);
+static int look_up_can_fd_speed (int speed);
 static void run_interactive ();
 static void print_bittiming();
 static void parse_bittiming();
@@ -73,35 +74,65 @@ struct termios  tios;
 
 int main (int argc, char *argv[])
 {
+  ldisc = N_TRIPLE;
+
   int             sp;
   int             opt;
   int             channel;
   int             run_as_daemon = 1;
   char           *pch;
   char           *tty = NULL;
-  char           *name[3];
-  int           speed = 250;
   char            buf[IFNAMSIZ + 1];
   char const     *devprefix = "/dev/";
 
-  ldisc = N_TRIPLE;
+  int             speed[3];
+  char           *name[3];
+  bool listen_only[3];
+  unsigned int user_speed[10];
+  bool can_fd = false;
+  bool iso_crc = false;
+  bool esi = false;
+  bool user_bittiming = false;
 
-  name[0] = NULL;
-  name[1] = NULL;
-  name[2] = NULL;
+  name[PORT_1] = NULL;
+  name[PORT_2] = NULL;
+  name[PORT_3] = NULL;
+
   ttypath[0] = '\0';
-  while ((opt = getopt(argc, argv, "s:n:l:iduvwh?fc:")) != -1)
+  const char delim[] = ":";
+  int i = 0;
+
+  char *tmp[3];
+
+  while ((opt = getopt(argc, argv, "s:n:l:duvwh?f:c:")) != -1)
   {
     switch (opt)
     {
     case 's'://set speed
-      speed = atoi(optarg);
+      tmp[i] = strtok(optarg, ":");
+      while (tmp[i] != NULL)
+      {
+        tmp[++i] = strtok(NULL, ":");
+      }
+      speed[PORT_1] = look_up_can_speed(strtol(tmp[0], NULL, 16));
+      speed[PORT_2] = look_up_can_speed(strtol(tmp[1], NULL, 16));
+      speed[PORT_3] = look_up_can_fd_speed(strtol(tmp[2], NULL, 16));
       break;
     case 'n'://set names
+      name[i] = strtok(optarg, ":");
+      while (name[i] != NULL)
+      {
+        name[++i] = strtok(NULL, ":");
+      }
       break;
     case 'l'://set listen-only
-      break;
-    case 'i': //interactive mode
+      tmp[i] = strtok(optarg, ":");
+      while (tmp[i] != NULL)
+      {
+        tmp[++i] = strtok(NULL, ":");
+      }
+      for (i = 0; i < 3; i++)
+        listen_only[i] = (bool)atoi(tmp[i]);
       break;
     case 'd'://run a deamon
       run_as_daemon = 0;
@@ -111,12 +142,25 @@ int main (int argc, char *argv[])
       {
         print_version(argv[2]);
       }
-    case 'w':// print FW version
-      break;
     case 'f':// CAN FD on
+      can_fd = true;
+      tmp[i] = strtok(optarg, ":");
+      while (tmp[i] != NULL)
+      {
+        tmp[++i] = strtok(NULL, ":");
+      }
+      esi = (bool)(atoi(tmp[0]));
+      iso_crc = (bool)(atoi(tmp[1]));
       break;
     case 'c':// User defined CAND FD bittiming
-      parse_bittiming();
+      user_bittiming = true;
+      tmp[i] = strtok(optarg, ":");
+      while (tmp[i] != NULL)
+      {
+        tmp[++i] = strtok(NULL, ":");
+      }
+      for (i = 0; i < 11; i++)
+        user_speed[i] = strtoul(tmp[i], NULL, 10);
       break;
     case 'u':
       print_bittiming();
@@ -131,7 +175,6 @@ int main (int argc, char *argv[])
       break;
     }
   }
-
   /* Initialize the logging interface */
   openlog(DAEMON_NAME, LOG_PID, LOG_LOCAL5);
 
@@ -139,13 +182,13 @@ int main (int argc, char *argv[])
   tty = argv[optind];
   if (NULL == tty)
     print_usage(argv[0]);
-
-  name[0] = argv[optind + 1];
-  if (name[0])
-    name[1] = argv[optind + 2];
-  if (name[1])
-    name[2] = argv[optind + 3];
-
+  /*
+    name[PORT_1] = argv[optind + 1];
+    if (name[PORT_1])
+      name[PORT_2] = argv[optind + 2];
+    if (name[PORT_2])
+      name[PORT_3] = argv[optind + 3];
+  */
   /* Prepare the tty device name string */
   pch = strstr(tty, devprefix);
   if (pch != tty)
@@ -196,11 +239,19 @@ int main (int argc, char *argv[])
 
   USB2CAN_TRIPLE_SendTimeStampMode(false, fd);
   sleep(1);
-  USB2CAN_TRIPLE_SendCANSpeed(1, speed, false, fd);
+  USB2CAN_TRIPLE_SendCANSpeed(1, speed[PORT_1], listen_only[PORT_1], fd);
   sleep(1);
-  USB2CAN_TRIPLE_SendCANSpeed(2, speed, false, fd);
+  USB2CAN_TRIPLE_SendCANSpeed(2, speed[PORT_2], listen_only[PORT_2], fd);
   sleep(1);
-  USB2CAN_TRIPLE_SendFDCANSpeed(2502000, false, false, false, fd);
+  if (!user_bittiming)
+  {
+    USB2CAN_TRIPLE_SendFDCANSpeed(speed[PORT_3], listen_only[PORT_3], esi, iso_crc, fd);
+  }
+  else
+  {
+    USB2CAN_TRIPLE_SendFDCANUsrSpeed(user_speed[NBRP], user_speed[NTSEG1], user_speed[NTSEG2], user_speed[NSJW], user_speed[DBRP], user_speed[DTSEG1], user_speed[DTSEG2], user_speed[DSJW], user_speed[TDCO], user_speed[TDCV], user_speed[TDCMOD], listen_only[PORT_3], esi, iso_crc, fd);
+  }
+
   sleep(1);
   USB2CAN_TRIPLE_GetFWVersion(fd);
   sleep(2);
@@ -311,19 +362,6 @@ static void print_version (char *prg)
 
 }
 
-static void run_interactive ()
-{
-  int c;
-
-  printf("Should this deamon run in foreground ? (y/n)");
-  c = getchar();
-  if (c != 0 && c == 'y')
-  {
-    printf("YOU PUT IN ");
-    putchar(c);
-  }
-}
-
 static void child_handler (int signum)
 {
   switch (signum)
@@ -348,36 +386,11 @@ static void child_handler (int signum)
 
 } /* END: child_handler() */
 
-static const char *look_up_can_speed (int speed)
-{
-  switch (speed)
-  {
-
-  case 10:   return "10 KBPS";
-  case 20:   return "20 KBPS";
-  case 33:   return "33 KBPS";
-  case 50:   return "50 KBPS";
-  case 62:   return "62 KBPS";
-  case 83:   return "83 KBPS";
-  case 100:   return "100 KBPS";
-  case 125:   return "125 KBPS";
-  case 250:   return "250 KBPS";
-  case 500:   return "500 KBPS";
-  case 1000:   return "1   MBPS";
-
-  default:  return "unknown";
-  }
-
-} /* END: look_up_can_speed() */
-/* END: print_version() */
-/*------------------------------------------------------------------------------------*/
-static void parse_bittiming()
-{
- //TODO
-}
 static void print_bittiming()
 {
   fprintf(stderr, "For detailed information see MCP2517FD datasheet\n");
+  fprintf(stderr, "No validation is provided, so be careful !!\n");
+  fprintf(stderr, "You can find online calculators\n");
   fprintf(stderr, "<bittiming options> --> ./tripled_64 -c[NBRP]:[NTSEG1]:[NTSEG2]:[NSJW]:[DBRP]:[DTSEG1]:[DTSEG2]:[DSJW]:[TDCO]:[TDCV]:[TDCMOD]\n");
   fprintf(stderr, "Nominal bittiming\n");
   fprintf(stderr, "NBRP - Nominal Baud Rate Prescaler\n");
@@ -396,67 +409,144 @@ static void print_bittiming()
   fprintf(stderr, "\n");
   exit(EXIT_FAILURE);
 }
+
+static int look_up_can_speed (int speed)
+{
+  switch (speed)
+  {
+
+  case 0x01:   return SPEED_10k;
+  case 0x02:   return SPEED_20k;
+  case 0x03:   return SPEED_33_3k;
+  case 0x04:   return SPEED_50k;
+  case 0x05:   return SPEED_62_5k;
+  case 0x06:   return SPEED_83_3k;
+  case 0x07:   return SPEED_100k;
+  case 0x08:   return SPEED_125k;
+  case 0x09:   return SPEED_250k;
+  case 0x0A:   return SPEED_500k;
+  case 0x0B:   return SPEED_1M;
+
+  default:  return 250;
+  }
+}
+
+static int look_up_can_fd_speed (int speed)
+{
+  switch (speed)
+  {
+  //nominal 125
+  case 0x01:   return CAN_125K_250K;
+  case 0x02:   return CAN_125K_500K;
+  case 0x03:   return CAN_125K_833K;
+  case 0x04:   return CAN_125K_1M;
+  case 0x05:   return CAN_125K_1M5;
+  case 0x06:   return CAN_125K_2M;
+  case 0x07:   return CAN_125K_3M;
+  case 0x08:   return CAN_125K_4M;
+  case 0x09:   return CAN_125K_5M;
+  case 0x0A:   return CAN_125K_6M7;
+  case 0x0B:   return CAN_125K_8M;
+  case 0x0C:   return CAN_125K_10M;
+
+  case 0x11:   return CAN_250K_500K;
+  case 0x12:   return CAN_250K_833K;
+  case 0x13:   return CAN_250K_1M;
+  case 0x14:   return CAN_250K_1M5;
+  case 0x15:   return CAN_250K_2M;
+  case 0x16:   return CAN_250K_3M;
+  case 0x17:   return CAN_250K_4M;
+  case 0x18:   return CAN_250K_5M;
+  case 0x19:   return CAN_250K_6M7;
+  case 0x1A:   return CAN_250K_8M;
+  case 0x1B:   return CAN_250K_10M;
+
+  case 0x21:   return CAN_500K_833K;
+  case 0x22:   return CAN_500K_1M;
+  case 0x23:   return CAN_500K_1M5;
+  case 0x24:   return CAN_500K_2M;
+  case 0x25:   return CAN_500K_3M;
+  case 0x26:   return CAN_500K_4M;
+  case 0x27:   return CAN_500K_5M;
+  case 0x28:   return CAN_500K_6M7;
+  case 0x29:   return CAN_500K_8M;
+  case 0x2A:   return CAN_500K_10M;
+
+  case 0x31:   return CAN_1000K_1M5;
+  case 0x32:   return CAN_1000K_2M;
+  case 0x33:   return CAN_1000K_3M;
+  case 0x34:   return CAN_1000K_4M;
+  case 0x35:   return CAN_1000K_5M;
+  case 0x36:   return CAN_1000K_6M7;
+  case 0x37:   return CAN_1000K_8M;
+  case 0x38:   return CAN_1000K_10M;
+
+  default:  return 2501000;
+  }
+}
 static void print_speed()
 {
   fprintf(stderr, "--------------------CAN 2.0--------------------\n");
-  fprintf(stderr, "               1 -> 10: 10 KBPS\n");
-  fprintf(stderr, "               2 -> 20: 20 KBPS\n");
-  fprintf(stderr, "               3 -> 33: 33 KBPS\n");
-  fprintf(stderr, "               5 -> 50 : 50 KBPS\n");
-  fprintf(stderr, "               6 -> 62: 62 KBPS\n");
-  fprintf(stderr, "               7 -> 83: 83 KBPS\n");
-  fprintf(stderr, "               8 -> 100: 100 KBPS\n");
-  fprintf(stderr, "               9 -> 125: 125 KBPS\n");
-  fprintf(stderr, "               A -> 250: 250 KBPS\n");
-  fprintf(stderr, "               B -> 500: 500 KBPS\n");
-  fprintf(stderr, "               C -> 1000: 1 MBPS\n");
+  fprintf(stderr, "               0x01 -> 10: 10 KBPS\n");
+  fprintf(stderr, "               0x02 -> 20: 20 KBPS\n");
+  fprintf(stderr, "               0x03 -> 33: 33 KBPS\n");
+  fprintf(stderr, "               0x04 -> 50 : 50 KBPS\n");
+  fprintf(stderr, "               0x05 -> 62: 62 KBPS\n");
+  fprintf(stderr, "               0x06 -> 83: 83 KBPS\n");
+  fprintf(stderr, "               0x07 -> 100: 100 KBPS\n");
+  fprintf(stderr, "               0x08 -> 125: 125 KBPS\n");
+  fprintf(stderr, "               0x09 -> 250: 250 KBPS\n");
+  fprintf(stderr, "               0x0A -> 500: 500 KBPS\n");
+  fprintf(stderr, "               0x0B -> 1000: 1 MBPS\n");
   fprintf(stderr, "-----------------------------------------------\n");
 
   fprintf(stderr, "-----------------CAN FD spped------------------\n");
-  fprintf(stderr, "               0  -> CAN_USR_SPEED = 0,\n");
-  fprintf(stderr, "               1  -> CAN_125K_250K = 125250\n");
-  fprintf(stderr, "               2  -> CAN_125K_500K = 125500\n");
-  fprintf(stderr, "               3  -> CAN_125K_833K = 125833\n");
-  fprintf(stderr, "               4  -> CAN_125K_1M   = 1251000\n");
-  fprintf(stderr, "               5  -> CAN_125K_1M5  = 1251500\n");
-  fprintf(stderr, "               6  -> CAN_125K_2M   = 1252000\n");
-  fprintf(stderr, "               7  -> CAN_125K_3M   = 1253000\n");
-  fprintf(stderr, "               8  -> CAN_125K_4M   = 1254000\n");
-  fprintf(stderr, "               9  -> CAN_125K_5M   = 1255000\n");
-  fprintf(stderr, "               A  -> CAN_125K_6M7  = 1256700\n");
-  fprintf(stderr, "               B  -> CAN_125K_8M   = 1258000\n");
-  fprintf(stderr, "               C  -> CAN_125K_10M  = 1259999\n");
-  fprintf(stderr, "               D  -> CAN_250K_500K = 250500\n");
-  fprintf(stderr, "               E  -> CAN_250K_833K = 250833\n");
-  fprintf(stderr, "               F  -> CAN_250K_1M   = 2501000\n");
-  fprintf(stderr, "              10  -> CAN_250K_1M5  = 2501500\n");
-  fprintf(stderr, "              11  -> CAN_250K_2M   = 2502000\n");
-  fprintf(stderr, "              12  -> CAN_250K_3M   = 2503000\n");
-  fprintf(stderr, "              13  -> CAN_250K_4M   = 2504000\n");
-  fprintf(stderr, "              14  -> CAN_250K_5M   = 2505000\n");
-  fprintf(stderr, "              15  -> CAN_250K_6M7  = 2506700\n");
-  fprintf(stderr, "              16  -> CAN_250K_8M   = 2508000\n");
-  fprintf(stderr, "              17  -> CAN_250K_10M  = 2509999\n");
-  fprintf(stderr, "              18  -> CAN_500K_833K = 500833\n");
-  fprintf(stderr, "              19  -> CAN_500K_1M   = 5001000\n");
-  fprintf(stderr, "              1A  -> CAN_500K_1M5  = 5001500\n");
-  fprintf(stderr, "              1B  -> CAN_500K_2M   = 5002000\n");
-  fprintf(stderr, "              1C  -> CAN_500K_3M   = 5003000\n");
-  fprintf(stderr, "              1D  -> CAN_500K_4M   = 5004000\n");
-  fprintf(stderr, "              1E  -> CAN_500K_5M   = 5005000\n");
-  fprintf(stderr, "              1F  -> CAN_500K_6M7  = 5006700\n");
-  fprintf(stderr, "              20  -> CAN_500K_8M   = 5008000\n");
-  fprintf(stderr, "              21  -> CAN_500K_10M  = 5009999\n");
-  fprintf(stderr, "              22  -> CAN_1000K_1M5 = 10001500\n");
-  fprintf(stderr, "              23  -> CAN_1000K_2M  = 10002000\n");
-  fprintf(stderr, "              24  -> CAN_1000K_3M  = 10003000\n");
-  fprintf(stderr, "              25  -> CAN_1000K_4M  = 10004000\n");
-  fprintf(stderr, "              26  -> CAN_1000K_5M  = 10005000\n");
-  fprintf(stderr, "              27  -> CAN_1000K_6M7 = 10006700\n");
-  fprintf(stderr, "              28  -> CAN_1000K_8M  = 10008000\n");
-  fprintf(stderr, "              29  -> CAN_1000K_10M = 10009999\n");
-  fprintf(stderr, "-----------------------------------------------\n");
+  fprintf(stderr, "0xXX  -> CAN_<nominal speed>_<data speed>\n");
+  fprintf(stderr, "               0x01  -> CAN_125K_250K\n");
+  fprintf(stderr, "               0x02  -> CAN_125K_500K\n");
+  fprintf(stderr, "               0x03  -> CAN_125K_833K \n");
+  fprintf(stderr, "               0x04  -> CAN_125K_1M\n");
+  fprintf(stderr, "               0x05  -> CAN_125K_1M5\n");
+  fprintf(stderr, "               0x06  -> CAN_125K_2M\n");
+  fprintf(stderr, "               0x07  -> CAN_125K_3M\n");
+  fprintf(stderr, "               0x08  -> CAN_125K_4M\n");
+  fprintf(stderr, "               0x09  -> CAN_125K_5M \n");
+  fprintf(stderr, "               0x0A  -> CAN_125K_6M7\n");
+  fprintf(stderr, "               0x0B  -> CAN_125K_8M\n");
+  fprintf(stderr, "               0x0C  -> CAN_125K_10M\n");
   fprintf(stderr, "\n");
+  fprintf(stderr, "               0x11  -> CAN_250K_500K\n");
+  fprintf(stderr, "               0x12  -> CAN_250K_833K\n");
+  fprintf(stderr, "               0x13  -> CAN_250K_1M\n");
+  fprintf(stderr, "               0x14  -> CAN_250K_1M5\n");
+  fprintf(stderr, "               0x15  -> CAN_250K_2M\n");
+  fprintf(stderr, "               0x16  -> CAN_250K_3M\n");
+  fprintf(stderr, "               0x17  -> CAN_250K_4M\n");
+  fprintf(stderr, "               0x18  -> CAN_250K_5M\n");
+  fprintf(stderr, "               0x19  -> CAN_250K_6M7\n");
+  fprintf(stderr, "               0x1A  -> CAN_250K_8M\n");
+  fprintf(stderr, "               0x1B  -> CAN_250K_10M\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "               0x21  -> CAN_500K_833K\n");
+  fprintf(stderr, "               0x22  -> CAN_500K_1M\n");
+  fprintf(stderr, "               0x23 -> CAN_500K_1M5\n");
+  fprintf(stderr, "               0x24  -> CAN_500K_2M\n");
+  fprintf(stderr, "               0x25  -> CAN_500K_3M\n");
+  fprintf(stderr, "               0x26  -> CAN_500K_4M\n");
+  fprintf(stderr, "               0x27  -> CAN_500K_5M\n");
+  fprintf(stderr, "               0x28  -> CAN_500K_6M7\n");
+  fprintf(stderr, "               0x29  -> CAN_500K_8M\n");
+  fprintf(stderr, "               0x2A  -> CAN_500K_10M\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "               0x31  -> CAN_1000K_1M5\n");
+  fprintf(stderr, "               0x32  -> CAN_1000K_2M\n");
+  fprintf(stderr, "               0x33  -> CAN_1000K_3M\n");
+  fprintf(stderr, "               0x34  -> CAN_1000K_4M\n");
+  fprintf(stderr, "               0x35  -> CAN_1000K_5M\n");
+  fprintf(stderr, "               0x36  -> CAN_1000K_6M7\n");
+  fprintf(stderr, "               0x37  -> CAN_1000K_8M\n");
+  fprintf(stderr, "               0x38  -> CAN_1000K_10M\n");
+  fprintf(stderr, "-----------------------------------------------\n");
   exit(EXIT_FAILURE);
 
 } /* END: print_usage() */
@@ -470,15 +560,12 @@ static void print_usage (char *prg)
   fprintf(stderr, "         -v                          (show version info)\n");
   fprintf(stderr, "         -t                          (show supported CAN 2.0 and CAN FD speeds)\n");
   fprintf(stderr, "         -u                          (show bittimng options and format)\n");
-  fprintf(stderr, "         -i                          (interactive mode))\n");
   fprintf(stderr, "         -s[port1]:[port2]:[port3]   (set speed: see CAN, see ./tripled_64 -t for available speeds)\n");
   fprintf(stderr, "         -n[port1]:[port2]:[port3]   (set name of interface)\n");
   fprintf(stderr, "         -l[1/0]:[1/0]:[1/0]         (listen-only mode )\n");
-  fprintf(stderr, "         -f                          (CAN FD on port 3)\n");
-  fprintf(stderr, "         -c[bittiming options]       (User defined CAN FD bittiming, see ./tripled_64 -u)\n");
+  fprintf(stderr, "         -f[1/0]:[1/0]               (CAN FD on port 3 -> [ESI]:[ISO_CRC]\n");
+  fprintf(stderr, "         -c<bittiming options>       (User defined CAN FD bittiming, see ./tripled_64 -u)\n");
   fprintf(stderr, "\nExamples:\n");
-  fprintf(stderr, "Deamon can be used in interactive mode or you can use parametrs\n");
-  fprintf(stderr, "Interactive mode : ./tripled_64 -i\n\n");
   fprintf(stderr, "tripled_64 -s1:2:3 /dev/ttyACM0\n");
   fprintf(stderr, "tripled_64 /dev/ttyACM0\n");
   fprintf(stderr, "tripled_64 dev/ttyACM0 -ncan0:can1:can2\n");
